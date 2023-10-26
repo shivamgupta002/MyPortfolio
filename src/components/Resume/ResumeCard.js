383c0e2b3932d9463f0386849433d0b21f21bde9
shivamgupta002 <shivamgupta992909@gmail.com>
2023-10-26 11:59:58 +0530

    Resume Update

---
+++ src/components/Resume/ResumeCard.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Card = (props) => {
+  return (
+    <>
+      <div className='box btn_shadow'>
+        <div className='title_content d_flex'>
+          <div className='title'>
+            <h2>{props.title}</h2>
+            <span>{props.year}</span>
+          </div>
+        </div>
+        <hr />
+        <p>{props.desc}</p>
+      </div>
+    </>
+  )
+}
+
+export default Card
