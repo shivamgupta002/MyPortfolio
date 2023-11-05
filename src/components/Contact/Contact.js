0dc06edbbfada518b7a8a38a29e1bad5c0f06098
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-05 16:34:28 +0530

    Contact Edit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,7 @@ const contact = () => {
   return (
     <>
       <div className="top" id="contact">
-        <h2>Feel free to contact Me Contact Me</h2>
+        <h2>Feel free to Contact Me</h2>
         <div className="contact">
           <div className="social">
             <a href="/" className="icon">
@@ -19,13 +19,19 @@ const contact = () => {
               <i class="fa-brands fa-instagram"></i>
               <p>shivamgupta9876</p>
             </a>
+            <a
+              href="https://www.linkedin.com/in/shivam-gupta-4b62a0206"
+              className="icon"
+            >
+              <i class="fa-brands fa-linkedin"></i>
+              <p>Linkedin</p>
+            </a>
           </div>
           <div className="social">
             <a href="/" className="icon">
               <i class="fa-solid fa-envelope"></i>
               <p>20shivamgupta02@gmail.com</p>
             </a>
-
             <a
               href="https://github.com/shivamgupta002"
               target="_blank"
@@ -33,7 +39,16 @@ const contact = () => {
               className="icon"
             >
               <i class="fa-brands fa-github"></i>
-              <p>shivamgupta002</p>
+              <p>Github</p>
+            </a>
+            <a
+              href="https://github.com/shivamgupta002"
+              target="_blank"
+              rel="noreferrer"
+              className="icon"
+            >
+              <i class="fa-brands fa-hackerrank"></i>
+              <p>HackerRank</p>
             </a>
           </div>
         </div>
